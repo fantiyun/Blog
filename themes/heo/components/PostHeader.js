b43d2b4be0877c375fe280995b95900594187750
Lacie Shields <129157916+fantiyun@users.noreply.github.com>
2024-07-23 07:49:28 +0800

    Update PostHeader.js

--- themes/heo/components/PostHeader.js
+++ themes/heo/components/PostHeader.js
@@ -118,11 +118,12 @@ export default function PostHeader({ post, siteInfo, isDarkMode }) {
                   </Link>
                 </>
               )}
-
-              // <div className='pl-1 mr-2'>
-              //   <i className='fa-regular fa-calendar-check'></i>{' '}
-              //   {post.lastEditedDay}
-              // </div>
+              {/* 
+                <div className='pl-1 mr-2'>
+                  <i className='fa-regular fa-calendar-check'></i>{' '}
+                  {post.lastEditedDay}
+                </div>
+              */}
             </div>
 
             {/* 阅读统计 */}
